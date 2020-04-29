6059eccab7fe32a5f5636990a8ddebeda815a666
ericdunipace <edunipace@g.harvard.edu>
2020-04-29 13:35:40 -0400

    project to hilbert curve

--- R/hilbert.projection.R
+++ R/hilbert.projection.R
@@ -1,7 +1,7 @@
 hilbert.projection <- function(X, Sigma = NULL) {
   X <- as.matrix(t(X))
   if(!is.null(Sigma)) {
-    X <- backsolve(chol(Sigma), X)
+    X <- backsolve(chol(Sigma), X, transpose = TRUE)
   }
   idx <- hilbert_proj_(X) + 1 # +1 to adjust C to R indexing
   return(idx)
